6fa4f25379646ac005d071f00266c076e8ac18f7
empi91 <korpet.filip@gmail.com>
2023-12-05 00:40:07 +0100

    Slicing string to minimum done

--- 01/01_02/main_02.py
+++ 01/01_02/main_02.py
@@ -13,30 +13,47 @@ letters_dict = {
     "nine": 9,
 }
 
+index_list = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+]
+
 file_path = Path("/home/empi/Advent_of_Code_2023/01/01_01/puzzle_input")
-
-
-def check_replace_string(line):
-    id_numbers = []
-    for key in letters_dict:
-        id_numbers.append(re.findall(key, line))
-        # if line.find(key) > -1:
-        #     id_numbers[key] = [line.find(key), len(key)]
-            # key_length = len(key)
-            # word = line[line.find(key):line.find(key) + key_length]
-            #
-            # prefix = line[:line.find(key)]
-            # number = str(letters_dict[key])
-            # suffix = line[line.find(key) + key_length:]
-            #
-            # new_line = prefix + number + suffix
-            # print(new_line)
-    print(id_numbers)
-    print("_________________")
-    return line
-
 
 with file_path.open(mode="r", encoding="utf-8") as file:
     for line in file:
-        check_replace_string(line)
-        #print(line)
+        first_index = [-1, -1]
+        last_index = [-1, -1]
+        for index in index_list:
+            position = line.find(index)
+            if position != -1:
+                if first_index[0] == -1 and last_index[0] == -1:
+                    first_index = [position, len(index)]
+                    last_index = [position, len(index)]
+                elif position < first_index[0]:
+                    first_index = [position, len(index)]
+                elif position > last_index[0]:
+                    last_index = [position, len(index)]
+
+        line = line[first_index[0]:first_index[0] + first_index[1]] + line[last_index[0]:last_index[0] + last_index[1]]
+
+
+
+        print(line)
+        print("____________")

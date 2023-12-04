a9e7ce9c819eef5ee7d9387b3c2c09d47aace6a6
empi91 <korpet.filip@gmail.com>
2023-12-04 23:51:07 +0100

    Leaving string slicing, starting with regex

--- 01/01_02/main_02.py
+++ 01/01_02/main_02.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 letters_dict = {
     "one": 1,
@@ -16,15 +17,22 @@ file_path = Path("/home/empi/Advent_of_Code_2023/01/01_01/puzzle_input")
 
 
 def check_replace_string(line):
+    id_numbers = []
     for key in letters_dict:
-        if line.find(key) > -1:
-            key_length = len(key)
-            word = line[line.find(key):line.find(key) + key_length]
-            #line.replace(word, str(letters_dict[key]))
-            print(f"Replacing: {word} with {letters_dict[key]}")
-            #print(line[line.find(key):])
-            #print(line[:line.find(key) + key_length])
-            print(line)
+        id_numbers.append(re.findall(key, line))
+        # if line.find(key) > -1:
+        #     id_numbers[key] = [line.find(key), len(key)]
+            # key_length = len(key)
+            # word = line[line.find(key):line.find(key) + key_length]
+            #
+            # prefix = line[:line.find(key)]
+            # number = str(letters_dict[key])
+            # suffix = line[line.find(key) + key_length:]
+            #
+            # new_line = prefix + number + suffix
+            # print(new_line)
+    print(id_numbers)
+    print("_________________")
     return line
 
 

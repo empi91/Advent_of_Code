ed3b3f7f68d507b438390c9a778bb1dfef9acb84
empi91 <korpet.filip@gmail.com>
2023-12-05 11:56:36 +0100

    01 done!

--- 01/01_02/main_02.py
+++ 01/01_02/main_02.py
@@ -34,6 +34,8 @@ index_list = [
     "9",
 ]
 
+
+
 file_path = Path("/home/empi/Advent_of_Code_2023/01/01_01/puzzle_input")
 
 with file_path.open(mode="r", encoding="utf-8") as file:
@@ -42,23 +44,22 @@ with file_path.open(mode="r", encoding="utf-8") as file:
         first_index = [-1, -1]
         last_index = [-1, -1]
         for index in index_list:
-            position = line.find(index)
-            if position != -1:
+            for match in re.finditer(index, line):
                 if first_index[0] == -1 and last_index[0] == -1:
-                    first_index = [position, len(index)]
-                    last_index = [position, len(index)]
-                elif position < first_index[0]:
-                    first_index = [position, len(index)]
-                elif position > last_index[0]:
-                    last_index = [position, len(index)]
+                    first_index = [match.span()[0], len(index)]
+                    last_index = [match.span()[0], len(index)]
+                elif match.span()[0] < first_index[0]:
+                    first_index = [match.span()[0], len(index)]
+                elif match.span()[0] > last_index[0]:
+                    last_index = [match.span()[0], len(index)]
 
         line = line[first_index[0]:first_index[0] + first_index[1]] + line[last_index[0]:last_index[0] + last_index[1]]
-        print(line)
-        print("________")
+
         for key, value in letters_dict.items():
             line = re.sub(key, str(value), line)
+        print(line)
+        print("________")
 
-
-        # value = int(line[0]) * 10 + int(line[1])
-        # score += value
-    #print(score)
+        value = int(line[0]) * 10 + int(line[1])
+        score += value
+    print(score)

494e2381dfdef959da64965cdc9d6a3b83d10360
empi91 <korpet.filip@gmail.com>
2023-12-05 15:33:22 +0100

    Work in progress

--- 03/03_01/03_01.py
+++ 03/03_01/03_01.py
@@ -7,12 +7,31 @@ file_path = Path.cwd() / "puzzle_input"
 
 def add_line(line, tab):
     line = re.sub("\n", "", line)
-    tab.append([line])
+    tab.append(line)
 
 
 def check_number(table):
     for line in table:
-        print(line)
+        for match in re.finditer(r'[0-9]+', line):
+            line_length = len(line)
+
+            # Check if symbol before
+            if match.span()[0] == 0:
+                #nothing before
+            else:
+                if (match.span()[0] - 1) != ".":
+                    #symbol before
+
+            # Check if symbol after
+            if match.span()[1] == line_length:
+                #nothing after
+            else:
+                if (match.span()[1] + 1) != ".":
+                    #symbol after
+
+            # Check if symbol in prev line
+            
+
     return 0
 
 
@@ -20,6 +39,6 @@ with file_path.open(mode="r", encoding="utf-8") as file:
     line_counter = 0
     for line in file:
         add_line(line, table)
-        
-    check_number(table)
 
+    check_number(table)
+print(table)

4fe87c5a04c2045d200bdc8449a8214448f676f6
empi91 <korpet.filip@gmail.com>
2023-12-13 11:38:56 +0100

    Still saving galaxy number as a separate chars

--- 11/11_01.py
+++ 11/11_01.py
@@ -6,7 +6,8 @@ file_path = Path.cwd() / "puzzle_input"
 
 data = []
 numbered_grid = []
-
+galaxies_coords = []
+score = 0
 
 def extend_rows(grid):
     start_search = 0
@@ -30,8 +31,34 @@ def replace_hash(grid, numbered_grid):
                 counter += 1
             else:
                 new_row += char
-        numbered_grid.append(new_row)
-    return counter
+        numbered_grid.append(list(new_row))
+    return counter - 1
+
+
+def check_galaxy_coordinates(galaxy):
+    for row in galaxy:
+        for item in row:
+            if item.isdigit():
+                galaxies_coords.append([galaxy.index(row), row.index(item)])
+
+    return 0
+
+
+def calc_distances(coords, scor):
+    count = 0
+    for galaxy in coords:
+        distance = 0
+        index = coords.index(galaxy)
+        row = galaxy[0]
+        col = galaxy[1]
+        for i in range(index + 1, len(coords)):
+            print(f"Pair: {numbered_grid[row][col]} and {numbered_grid[coords[i][0]][coords[i][1]]}")
+            distance += abs(coords[i][0] - row)
+            distance += abs(coords[i][1] - col)
+            count += 1
+        scor += distance
+    print(count)
+    return scor
 
 
 with file_path.open(mode="r", encoding="utf-8") as file:
@@ -42,7 +69,13 @@ extend_rows(data)
 transposed_list = list(map(list, zip(*data)))
 extend_rows(transposed_list)
 grid = list(map(list, zip(*transposed_list)))
-counter = replace_hash(grid, numbered_grid)
+# counter = replace_hash(grid, numbered_grid)
+# check_galaxy_coordinates(numbered_grid)
+# score = calc_distances(galaxies_coords, score)
 
-for line in numbered_grid:
+for line in grid:
     print(line)
+
+# print(galaxies_coords)
+# print(counter)
+print(score)

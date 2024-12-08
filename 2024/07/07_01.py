8a8bd7228ebc163bf0d681170c77865f8df5b6e3
empi91 <korpet.filip@gmail.com>
2024-12-08 11:44:16 +0100

    07 DONE

---
+++ 2024/07/07_01.py
@@ -0,0 +1,36 @@
+import re
+
+operations = []
+score = 0
+
+with open("puzzle_input") as file:
+    for line in file:
+        operations.append([int(x) for x in re.split(": | ", line)])
+
+
+for op in operations:
+    result = op[0]
+    queue = []
+
+    for i in range(1, len(op)):
+        if not queue: 
+            queue.append(op[i])
+        else:
+            q1 = []
+            for value in queue:
+                temp = value
+
+                q1.append(temp + op[i])
+                q1.append(temp * op[i])
+
+            queue.clear()
+
+            for item in q1:
+                if item <= result:
+                    queue.append(item)
+
+        
+    if result in queue: 
+        score += result
+
+print(score)
